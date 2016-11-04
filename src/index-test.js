a0f66f21e973ab8f18d4fa9760401f890706fb8f
Justin Falcone <justin.falcone@buzzfeed.com>
2016-11-04 09:14:18 -0400

    modernize

--- src/index-test.js
+++ src/index-test.js
@@ -1,118 +1,118 @@
-import test from "tape"
-import fs from "fs"
-import peg from "pegjs"
+const test = require('tape')
+const fs = require('fs')
+const peg = require('pegjs')
 
-const data = fs.readFileSync("./src/index.peg","utf8")
-const theParser = peg.buildParser(data)
+const data = fs.readFileSync('./src/index.peg', 'utf8')
+const theParser = peg.generate(data)
 
-function parse (str, parseTree) {
-    return this.deepEqual(theParser.parse(str),parseTree)
+function parse (t, str, parseTree) {
+    return t.deepEqual(theParser.parse(str), parseTree)
 }
 
-function failParse (str) {
-    return this.throws(() => theParser.parse(str))
+function failParse (t, str) {
+    return t.throws(() => theParser.parse(str))
 }
 
 const types = {
-    Number: "Number",
-    Binding: "Binding",
-    String: "String",
-    FuncCall: "FuncCall",
+    Number: 'Number',
+    Binding: 'Binding',
+    String: 'String',
+    FuncCall: 'FuncCall'
 }
 
-test("Number", (assert) => {
-    assert::parse(`123`,
-        [types.Number,123])
+test('Number', (t) => {
+    parse(t, `123`,
+        [types.Number, 123])
 
-    assert::parse(`-42`,
-        [types.Number,-42])
-    assert::parse(`0.5`,
-        [types.Number,0.5])
-    assert::parse(`.2`,
-        [types.Number,0.2])
-    assert::parse(`-.4`,
-        [types.Number,-0.4])
+    parse(t, `-42`,
+        [types.Number, -42])
+    parse(t, `0.5`,
+        [types.Number, 0.5])
+    parse(t, `.2`,
+        [types.Number, 0.2])
+    parse(t, `-.4`,
+        [types.Number, -0.4])
 
-    assert::failParse(`4.`)
-    assert::failParse(`6-2`)
-    assert::failParse(`3.2.5`)
-    assert.end()
+    failParse(t, `4.`)
+    failParse(t, `6-2`)
+    failParse(t, `3.2.5`)
+    t.end()
 })
 
-test("Binding",(assert) => {
-    assert::parse(`x`,
-        [types.Binding,"x"])
-    assert::parse(`+`,
-        [types.Binding,"+"])
-    assert::parse(`foo_123`,
-        [types.Binding,"foo_123"])
+test('Binding', (t) => {
+    parse(t, `x`,
+        [types.Binding, 'x'])
+    parse(t, `+`,
+        [types.Binding, '+'])
+    parse(t, `foo_123`,
+        [types.Binding, 'foo_123'])
 
-    assert::failParse(`123foo`)
-    assert.end()
+    failParse(t, `123foo`)
+    t.end()
 })
 
-test("FuncCall",(assert) => {
-    assert::parse(`foo 1.5`,
+test('FuncCall', (t) => {
+    parse(t, `foo 1.5`,
         [types.FuncCall,
-            [types.Binding,"foo"],
-            [types.Number,1.5]])
+            [types.Binding, 'foo'],
+            [types.Number, 1.5]])
 
-    assert::parse(`foo bar`,
+    parse(t, `foo bar`,
         [types.FuncCall,
-            [types.Binding,"foo"],
-            [types.Binding,"bar"]])
+            [types.Binding, 'foo'],
+            [types.Binding, 'bar']])
 
-    assert::parse(`foo 1.5, bar, -3`,
+    parse(t, `foo 1.5, bar, -3`,
         [types.FuncCall,
-            [types.Binding,"foo"],
-            [types.Number,1.5],
-            [types.Binding, "bar"],
+            [types.Binding, 'foo'],
+            [types.Number, 1.5],
+            [types.Binding, 'bar'],
             [types.Number, -3]])
 
-    assert::parse(`foo 10, bar 3, 8`,
+    parse(t, `foo 10, bar 3, 8`,
         [types.FuncCall,
-            [types.Binding, "foo"],
-            [types.Number,10],
+            [types.Binding, 'foo'],
+            [types.Number, 10],
             [types.FuncCall,
-                [types.Binding, "bar"],
-                [types.Number,3],
-                [types.Number,8]]])
+                [types.Binding, 'bar'],
+                [types.Number, 3],
+                [types.Number, 8]]])
 
-    assert::failParse(`foo 10,`)
+    failParse(t, `foo 10,`)
 
-    assert.end()
+    t.end()
 })
 
-test("Grouping",(assert) => {
-    assert::parse(`(1)`,
+test('Grouping', (t) => {
+    parse(t, `(1)`,
         [types.Number, 1])
-    assert::parse(`(foo)`,
-        [types.Binding,"foo"])
-    assert::parse(`(+ 1, 2)`,
+    parse(t, `(foo)`,
+        [types.Binding, 'foo'])
+    parse(t, `(+ 1, 2)`,
         [types.FuncCall,
-            [types.Binding,"+"],
-            [types.Number,1],
-            [types.Number,2]])
+            [types.Binding, '+'],
+            [types.Number, 1],
+            [types.Number, 2]])
 
-    assert::parse(`foo 10, (bar 3), 8`,
+    parse(t, `foo 10, (bar 3), 8`,
         [types.FuncCall,
-            [types.Binding, "foo"],
-            [types.Number,10],
+            [types.Binding, 'foo'],
+            [types.Number, 10],
             [types.FuncCall,
-                [types.Binding, "bar"],
-                [types.Number,3]],
-            [types.Number,8]])
+                [types.Binding, 'bar'],
+                [types.Number, 3]],
+            [types.Number, 8]])
 
-    assert::parse(`foo (bar (baz 10), 12), quxx`,
+    parse(t, `foo (bar (baz 10), 12), quxx`,
         [types.FuncCall,
-            [types.Binding, "foo"],
+            [types.Binding, 'foo'],
             [types.FuncCall,
-                [types.Binding, "bar"],
+                [types.Binding, 'bar'],
                 [types.FuncCall,
-                    [types.Binding,"baz"],
+                    [types.Binding, 'baz'],
                     [types.Number, 10]],
                 [types.Number, 12]],
-            [types.Binding, "quxx"]])
+            [types.Binding, 'quxx']])
 
-    assert.end()
+    t.end()
 })
